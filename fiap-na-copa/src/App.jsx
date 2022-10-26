ba055444fd3d99d69adfda5e9903851e0de0c448
nathslopes <nathalia-silvalopes@outlook.com>
2022-10-26 00:00:13 -0300

    Criado componente App.

---
+++ fiap-na-copa/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Menu from './components/Menu';
+import MainRoutes from './routes/MainRoutes';
+
+
+function App() {
+
+
+    return (
+        <>
+            <Menu/>
+            <MainRoutes/>
+        </>
+    );
+}
+
+export default App;
